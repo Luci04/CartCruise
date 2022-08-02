6816ce1495b9d36505fe10ff92ec973a6c536c23
luci04 <avinashukla2000@gmail.com>
2022-08-02 21:52:36 +0530

    Ratings Section Added

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import Rating from "./Rating";
 
 const Product = ({ product }) => {
   return (
@@ -15,12 +16,15 @@ const Product = ({ product }) => {
         </a>
 
         <Card.Text as="div">
-          <div className="my-3">
-            {product.rating} from {product.numReviews} reviews
-          </div>
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
         </Card.Text>
 
-        <Card.Text as="h3">{product.price}</Card.Text>
+        <Card.Text as="h3">
+          ₹{parseFloat(product.price * 70).toFixed(2)}
+        </Card.Text>
       </Card.Body>
     </Card>
   );
