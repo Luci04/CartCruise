a971d1147542ccaa82405ea9079f1ecfae648034
luci04 <avinashukla2000@gmail.com>
2022-08-03 22:30:49 +0530

    Error Management

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,38 @@
+import e from "express";
+import express from "express";
+import AsyncHandler from "express-async-handler";
+
+const router = express.Router();
+
+import Product from "../models/productModel.js";
+
+//@desc Fetch All Product
+//@route GET /api/product
+//@access Public
+router.get(
+  "/",
+  AsyncHandler(async (req, res) => {
+    const product = await Product.find({});
+    res.json(product);
+  })
+);
+
+//@desc Fetch Single Product
+//@route GET /api/product/:id
+//@access Public
+
+router.get(
+  "/:id",
+  AsyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404);
+      throw new Error("Product not found");
+    }
+  })
+);
+
+export default router;
